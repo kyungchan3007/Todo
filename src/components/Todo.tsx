da7ec35cacc4ad8214e2fffe4ae5a318729afc2a
kyungchandevelop@gmail.com <championcom@naver.com>
2023-02-05 01:34:25 +0900

    categoryState

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,14 +1,36 @@
+import { useSetRecoilState } from "recoil";
 import { IToDo } from "../type/todoType";
+import { toDoState } from "../unit/recoilState";
 
 export default function Todo(props: IToDo) {
+  //   const onClickTodo = (category: IToDo["category"]) => {
+  //     console.log(category);
+  //   };
+  const setToDos = useSetRecoilState(toDoState);
+  const onClickTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = e;
+  };
   return (
     <>
       <div>
         <li key={props.id}>
-          {props.text} {props.id}
-          <button>Todo</button>
-          <button>Doing</button>
-          <button>Done</button>
+          {props.text}
+          {props.category !== "TO_DO" && (
+            // <button
+            //   onClick={() => {
+            //     onClickTodo("TO_DO"); // onClick함수에 인자값을 넣어야할때
+            //   }}
+            // >
+            //   Todo
+            // </button>
+            <button onClick={onClickTodo}></button>
+          )}
+          {props.category !== "DOING" && (
+            <button onClick={onClickTodo}></button>
+          )}
+          {props.category !== "DONE" && <button onClick={onClickTodo}></button>}
         </li>
       </div>
     </>
